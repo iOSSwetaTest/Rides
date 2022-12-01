b3a391cdce94dca6690adc1d1a3525f92fb1d9b0
iOSSwetaTest <swetadodiya2021@gmail.com>
2022-12-01 12:07:40 -0700

    1st commit

---
+++ Rides/Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Sweta Dodiya on 2022-12-01.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
